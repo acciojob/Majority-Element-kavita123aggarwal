bf3cef737ccf38cb7279f5e81222f3065a0b332e
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-09-18 22:30:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function findDuplicates(arr) {
     return self.indexOf(value) !== index;
   });
 }
-
+const myArray = [2, 1, 2];
+const duplicates = findDuplicates(myArray);
+console.log(duplicates);
