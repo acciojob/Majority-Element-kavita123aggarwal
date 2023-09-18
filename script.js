5b0687b93d07e6af52ea2e8585fd5fbebb1123bf
kavita123aggarwal <118376629+kavita123aggarwal@users.noreply.github.com>
2023-09-18 22:32:14 +0530

    created a submission

--- script.js
+++
@@ -1,8 +0,0 @@
-function findDuplicates(arr) {
-  return arr.filter((value, index, self) => {
-    return self.indexOf(value) !== index;
-  });
-}
-const myArray = [2, 1, 2];
-const duplicates = findDuplicates(myArray);
-console.log(duplicates);
